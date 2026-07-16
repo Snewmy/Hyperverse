c9c9725bb474e08631f0c70045e524abcde6fb56
Snewmy <snewmy8@hotmail.com>
2026-07-16 16:59:22 -0500

    Add 26.2 support

---
+++ hyperverse-nms-26-2/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("hyperverse.base-conventions")
+    alias(libs.plugins.paperweight.userdev)
+}
+
+indra {
+    javaVersions {
+        minimumToolchain(25)
+        target(25)
+    }
+}
+
+dependencies {
+    paperweight.paperDevBundle("26.2.build.+")
+    compileOnly(projects.hyperverseNmsCommon)
+}
